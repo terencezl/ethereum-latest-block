162e6b84d2f81edae27a599ae56b063dea2b6a1f
Terence Z. Liu <terence.z.liu@gmail.com>
2023-03-30 22:45:39 -0400

    socketio server/client

--- backend.py
+++ backend.py
@@ -1,56 +1,121 @@
-import gevent
-from gevent import monkey
-monkey.patch_all()
-import time
+import eventlet
+eventlet.monkey_patch()
+import argparse
 from web3 import Web3
 from datetime import datetime
 import requests
 from rich import print
-from concurrent import futures
-# from decimal import Decimal
+from flask import Flask, Response, request
+from flask.templating import render_template
+from flask_socketio import SocketIO, emit
+from loguru import logger
 
-uri_eth_node = "https://eth-mainnet.alchemyapi.io/v2/GNauZOAEhjOc34zQQqQuXorOlmC6wJ6W"
-w3 = Web3(Web3.HTTPProvider(uri_eth_node))
-latest_block = w3.eth.get_block('latest')
+URI_ETH_NODE = "https://eth-mainnet.alchemyapi.io/v2/GNauZOAEhjOc34zQQqQuXorOlmC6wJ6W"
+URI_ETH2USD = "https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD"
 
-ts = latest_block["timestamp"]
-ts_iso = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+w3 = Web3(Web3.HTTPProvider(URI_ETH_NODE))
+pool = eventlet.GreenPool(512)
 
-uri_eth2usd_ts = f"https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts={ts}"
-r = requests.get(uri_eth2usd_ts)
-price_at_ts = r.json()["ETH"]["USD"]
-# note this price is in float, not Decimal
-# if this is a problem, need to get exact price at timestamp
-print(f"Timestamp: {ts_iso} Block Number: {latest_block['number']} Price: {price_at_ts}")
 
-def get_transaction(tx_hash):
-    tx = w3.eth.get_transaction(tx_hash)
-    return tx
+def get_price_at_ts(ts):
+    # note this price is in float, not Decimal
+    # if this is a problem, need to get exact price at timestamp
+    r = requests.get(f"{URI_ETH2USD}&ts={ts}")
+    return r.json()["ETH"]["USD"]
 
-# thread_pool = futures.ThreadPoolExecutor(max_workers=512)
 
-# t = time.perf_counter()
-# futures = []
-# for t_hex in latest_block["transactions"]:
-#     futures.append(thread_pool.submit(get_transaction, t_hex))
+def check_latest_block():
+    latest_block = w3.eth.get_block('latest')
+    return latest_block["number"]
 
-# for future in futures:
-#     tx = future.result()
-#     val_eth = w3.from_wei(tx.get('value', None), 'ether')
-#     val_usd = float(val_eth) * price_at_ts
-#     print(f"From: {tx.get('from', None)} To: {tx.get('to', None)} Value: {val_eth:20f} ETH Value: {val_usd:20f} USD")
 
-# print(f"Time: {time.perf_counter() - t:0.4f} seconds")
-# print("-" * 80)
+def get_latest_block(block_number="latest"):
+    latest_block = w3.eth.get_block(block_number)
 
-# green threads
-pool = gevent.pool.Pool(512)
+    payload = {}
+    ts = latest_block["timestamp"]
+    ts_iso = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    bn = latest_block["number"]
+    price_at_ts = get_price_at_ts(ts)
+    payload["ts"] = ts_iso
+    payload["bn"] = bn
+    payload["price"] = price_at_ts
+    logger.debug(f"Timestamp: {ts_iso} Block Number: {bn} Price: {price_at_ts}")
 
-t = time.perf_counter()
-transactions = pool.map(get_transaction, latest_block["transactions"])
+    payload["txs"] = []
+    # nul_to_addr = None
+    for tx in pool.imap(w3.eth.get_transaction, latest_block["transactions"]):
+        # sometimes "to" field could be None
+        # need to investigate but handle broardly here for now
+        amt_eth = w3.from_wei(tx["value"], 'ether')
+        amt_usd = float(amt_eth) * price_at_ts
 
-for tx in transactions:
-    val_eth = w3.from_wei(tx.get('value', None), 'ether')
-    val_usd = float(val_eth) * price_at_ts
-    print(f"From: {tx.get('from', None)} To: {tx.get('to', None)} Value: {val_eth:20f} ETH Value: {val_usd:20f} USD")
-print(f"Time: {time.perf_counter() - t:0.4f} seconds")
+        payload["txs"].append({
+            "addr_from": tx.get('from', " " * 42),
+            "addr_to": tx.get('to', " " * 42),
+            "amt_eth": f"{amt_eth:20f}",
+            "amt_usd": f"{amt_usd:20f}"
+        })
+        # logger.info(f"From: {tx.get('from', ' ' * 42)} To: {tx.get('to', ' ' * 42)} Value: {amt_eth:20f} ETH Value: {amt_usd:20f} USD")
+        # if tx.get('to', None) is None:
+        #     nul_to_addr = tx.get('to', " " * 42)
+
+    # if nul_to_addr:
+    #     logger.warning("Found null address in latest block. Investigate!")
+
+    return payload
+
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+session_ids = set()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("connect")
+def handle_connect():
+    session_id = request.sid
+    session_ids.add(session_id)
+    logger.info(f"Client {session_id} connected.")
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    session_id = request.sid
+    session_ids.remove(session_id)
+    logger.info(f"Client {session_id} disconnected.")
+
+
+@socketio.on("get_latest_block")
+def handle_get_latest_block():
+    session_id = request.sid
+    logger.info(f"Client {session_id} requested latest block.")
+
+    last_bn = -1
+    while True:
+        if session_id not in session_ids:
+            logger.info(f"Client {session_id} already disconnected. Stopping loop.")
+            break
+
+        latest_bn = check_latest_block()
+        if latest_bn > last_bn:
+            logger.info(f"New block {latest_bn} found. Sending to client.")
+            payload = get_latest_block()
+            last_bn = payload["bn"]
+            emit("latest_block", payload)
+        else:
+            eventlet.sleep(1)
+
+
+if __name__ == "__main__":
+    # for i in range(16944200, 16944244):
+    #     logger.info(f"Getting block {i}...")
+    #     get_latest_block(i)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=5000)
+    args = parser.parse_args()
+    socketio.run(app, host=args.host, port=args.port)
